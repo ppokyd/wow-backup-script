7c2c9fda35d44710c32de09d01424b327bbb2716
Pete <petro.pokydko@gmail.com>
2022-02-22 12:52:10 +0200

    additional folder template

--- backup.ps1
+++ backup.ps1
@@ -18,9 +18,10 @@ If (!(test-path $wow_folder)) {
   Exit
 }
 
-$wtfFolder = "$($wow_folder)\_retail_\WTF\"
-$uiFolder = "$($wow_folder)\_retail_\Interface\"
-$ssFolder = "$($wow_folder)\_retail_\Screenshots\"
+$wtf_folder = "$($wow_folder)\_retail_\WTF\"
+$ui_folder = "$($wow_folder)\_retail_\Interface\"
+$ss_folder = "$($wow_folder)\_retail_\Screenshots\"
+# $new_folder = "$($wow_folder)\_retail_\New Folder\"
 
 If (!(test-path $backup_folder)) {
   LogWrite "Backup folder - creating"
@@ -31,26 +32,33 @@ If (!(test-path $backup_folder)) {
 $OriginalPref = $ProgressPreference
 $ProgressPreference = "SilentlyContinue"
 
+# If (test-path $new_folder) {
+#   LogWrite "New folder - backup start"
+#   Compress-Archive -Path $new_folder -DestinationPath "$($backup_folder)\new_folder" -CompressionLevel "NoCompression" -Force
+#   LogWrite "New folder - backup done"
+# } else {
+#   LogWrite "New folder - not found"
+# }
 
-If (test-path $wtfFolder) {
+If (test-path $wtf_folder) {
   LogWrite "WTF folder - backup start"
-  Compress-Archive -Path $wtfFolder -DestinationPath "$($backup_folder)\wtf" -CompressionLevel "NoCompression" -Force
+  Compress-Archive -Path $wtf_folder -DestinationPath "$($backup_folder)\wtf" -CompressionLevel "NoCompression" -Force
   LogWrite "WTF folder - backup done"
 } else {
   LogWrite "WTF folder - not found"
 }
 
-If (test-path $uiFolder) {
+If (test-path $ui_folder) {
   LogWrite "UI folder - backup start"
-  Compress-Archive -Path $uiFolder -DestinationPath "$($backup_folder)\interface" -CompressionLevel "NoCompression" -Force
+  Compress-Archive -Path $ui_folder -DestinationPath "$($backup_folder)\interface" -CompressionLevel "NoCompression" -Force
   LogWrite "UI folder - backup done"
 } else {
   LogWrite "UI folder - not found"
 }
 
-If (test-path $ssFolder) {
+If (test-path $ss_folder) {
   LogWrite "UI Screenshots - backup start"
-  Compress-Archive -Path $ssFolder -DestinationPath "$($backup_folder)\Screenshots" -CompressionLevel "NoCompression" -Force
+  Compress-Archive -Path $ss_folder -DestinationPath "$($backup_folder)\Screenshots" -CompressionLevel "NoCompression" -Force
   LogWrite "UI Screenshots - backup done"
 }
 
